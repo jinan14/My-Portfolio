cd4d8f9bf2b6311c10fa2ba340fdec0337d1fa10
jinan14 <jinanghanam@gmail.com>
2024-11-24 07:51:53 -0800

    personal website

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,6 @@
 
 
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
